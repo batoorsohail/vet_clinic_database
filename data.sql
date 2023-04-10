c9cac8814e609db721112b4843a9f48180e3b353
unknown <sohailshahidi1435@gmail.com>
2023-04-10 21:30:06 +0430

    Insert data

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-/* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES('Agumon', '02 03 2020', 10.23, true, 2),
+      ('Gabumon', '11 15 2018', 8, true, 2),
+      ('Pikachu', '06 07 2021', 15.04, false, 1),
+      ('Devimon', '05 12 2017', 11, true, 5);
