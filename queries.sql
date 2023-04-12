4dc5fd6f1be7fc4f894a083b36707dc5d01499ad
unknown <sohailshahidi1435@gmail.com>
2023-04-12 08:59:30 +0430

    Add new queries

--- queries.sql
+++ queries.sql
@@ -36,3 +36,9 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 
 SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1 1 1990' AND '1 1 2000' GROUP BY species;
