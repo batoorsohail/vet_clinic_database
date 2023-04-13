18391475cc15e0551565969ac72d2248dc08b19f
unknown <sohailshahidi1435@gmail.com>
2023-04-13 16:20:48 +0430

    Add new queries

--- queries.sql
+++ queries.sql
@@ -48,3 +48,26 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1 1 1990' AND '1 1 2000' GROUP BY species;
+
+SELECT name FROM animals JOIN owners ON animals.owner_id = (SELECT owners.id WHERE full_name = 'Melody Pond');
+
+SELECT animals.name FROM animals JOIN species ON animals.species_id = (SELECT species.id WHERE species.name = 'Pokemon');
+
+SELECT animals.name, owners.full_name FROM animals
+RIGHT JOIN owners ON animals.owner_id = owners.id;
+
+SELECT species.name, COUNT(animals.name) FROM animals
+JOIN species ON animals.species_id = species.id GROUP BY species.name;
+
+SELECT animals.name FROM animals
+JOIN owners ON animals.owner_id = (SELECT owners.id WHERE owners.full_name = 'Jennifer Orwell')
+JOIN species ON animals.species_id = (SELECT species.id WHERE species.name = 'Digimon');
+
+SELECT animals.name, animals.escape_attempts FROM animals
+JOIN owners ON animals.owner_id = (SELECT owners.id WHERE owners.full_name = 'Dean Winchester')
+AND animals.escape_attempts = 0;
+
+SELECT owners.full_name FROM owners
+JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY COUNT(*) DESC LIMIT 1;
