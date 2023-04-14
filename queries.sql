663d063d97ab606916c88fe4385a0a0ea6e18d98
unknown <sohailshahidi1435@gmail.com>
2023-04-14 16:37:36 +0430

    Add new queries

--- queries.sql
+++ queries.sql
@@ -78,3 +78,55 @@ SELECT owners.full_name FROM owners
 JOIN animals ON owners.id = animals.owner_id
 GROUP BY owners.full_name
 ORDER BY COUNT(*) DESC LIMIT 1;
+
+SELECT animals.name FROM animals
+JOIN visits ON animals.id = visits.animals_id
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'William Tatcher'
+ORDER BY visits.date_of_visit DESC LIMIT 1;
+
+SELECT COUNT(animals.name) FROM animals
+JOIN visits ON animals.id = visits.animals_id
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Stephanie Mendez';
+
+SELECT vets.name, species.name
+FROM vets
+LEFT JOIN specializations ON vets.id = vet_id
+LEFT JOIN species ON species.id = species_id;
+
+SELECT animals.name
+FROM animals
+JOIN visits ON animals.id = animals_id AND date_of_visit >= '2020-04-01' AND date_of_visit <= '2020-08-30'
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Stephanie Mendez';
+
+SELECT animals.name
+FROM animals
+JOIN visits ON animals.id = animals_id
+GROUP BY animals.name
+ORDER BY COUNT(*) DESC LIMIT 1;
+
+SELECT animals.name
+FROM animals
+JOIN visits ON animals.id = visits.animals_id
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Maisy Smith'
+ORDER BY visits.date_of_visit ASC LIMIT 1;
+
+SELECT animals.name, species.name, vets.name, visits.date_of_visit FROM visits
+JOIN animals ON animals.id = visits.animals_id
+JOIN vets ON vets.id = visits.vet_id
+JOIN species ON species.id = animals.species_id
+WHERE visits.date_of_visit = (SELECT MAX(date_of_visit) FROM visits);
+
+SELECT COUNT(date_of_visit) FROM visits
+JOIN animals ON animals.id = visits.animals_id
+WHERE vet_id NOT IN (SELECT vet_id FROM specializations WHERE species_id = animals.species_id);
+
+SELECT species.name FROM visits
+JOIN vets ON visits.vet_id = (SELECT vet_id FROM vets WHERE vets.name = 'Maisy Smith')
+JOIN animals ON animals.id = visits.animals_id
+JOIN species ON species.id = animals.species_id
+GROUP BY species.name
+ORDER BY COUNT(*) DESC LIMIT 1;
